739fd3f2251f6f31780fcc8904bdec6cec1918a3
htw0835 <htw0835@stacmail.net>
2020-02-04 10:10:56 +1300

    Updated

---
+++ hello world.py
@@ -0,0 +1,3 @@
+""" I'm so smart and have such a huge veiny brain. I made this all on my own on my very first try. I even figured out how to join strings. Put me in year 13 digi already, Phil. """
+#This makes the words show up.
+print("Hello world!"+" I'm experimenting!")
---
+++ whats your name.py
@@ -0,0 +1,4 @@
+#Set input to name
+name = input("What's your name? >")
+#Greet the user along with their name
+print("Hello "+name)
